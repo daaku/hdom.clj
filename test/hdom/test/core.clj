207c7c7af6dea92ee8c346fd939952cc823451df
Naitik Shah <n@daaku.org>
2011-12-28 19:49:05 +1000

    test of a more complex structure

--- test/hdom/test/core.clj
+++ test/hdom/test/core.clj
@@ -24,3 +24,22 @@
     (is (= "a" tag))
     (is (= "i" (:id attrs)))
     (is (= "y z" (:class attrs)))))
+
+(deftest reduce-elements-multiple-complex
+  (let [s-content "hello"
+        input [[:h1#i.y.z] [:em#d.e [:strong.f s-content]]]
+        [memo elements] (reduce-elements nil input el-identity)
+        [h1 h1-attrs] (first elements)
+        [em em-attrs em-content] (second elements)
+        [[strong strong-attrs strong-content]] em-content]
+    (is (= nil memo))
+    (is (= "h1" h1))
+    (is (= "i" (:id h1-attrs)))
+    (is (= "y z" (:class h1-attrs)))
+    (is (= "em" em))
+    (is (= "d" (:id em-attrs)))
+    (is (= "e" (:class em-attrs)))
+    (is (= "strong" strong))
+    (is (= nil (:id strong-attrs)))
+    (is (= "f" (:class strong-attrs)))
+    (is (= s-content (first strong-content)))))
